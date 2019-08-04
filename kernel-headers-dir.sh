ef726091311689ff9a4b72df64441662f48612a9
tsurovec <tsurovec@localhost.localdomain>
2019-08-04 17:43:43 +0200

    kernel-headers-dir.sh - drop trailing -generic from kernel version

--- kernel-headers-dir.sh
+++ kernel-headers-dir.sh
@@ -1,5 +1,5 @@
 for dir in /usr/src/kernels/ /usr/src/linux-headers-; do
-	path="$dir$(uname -r)"
+	path="$dir$(uname -r | sed 's/\(.*\)-generic/\1/')"
         [ -d $path ] && echo $path && exit 0
 done
 exit 1
