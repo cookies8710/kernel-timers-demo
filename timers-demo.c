2a45c27bf923b3679037186c6a0bf72895d963dc
tsurovec <tsurovec@localhost.localdomain>
2019-08-05 21:15:23 +0200

    data, dyn alloc

--- timers-demo.c
+++ timers-demo.c
@@ -15,16 +15,25 @@
 static int td_counter = 0;
 static void td_timer_func(struct timer_list* timer);
 
-DEFINE_TIMER(td_timer, td_timer_func);
+struct td_my_struct {
+	int data1;
+	struct timer_list tmr;
+	const char *str;
+};
+
+//struct td_my_struct td_instance;
 
 static void td_timer_func(struct timer_list* timer)
 {
+	struct td_my_struct *ms = from_timer(ms, timer, tmr);
+	TD_LOG("td_timer_func, ms->data: %d", ms->data1);
+
 	unsigned long tgt = jiffies + msecs_to_jiffies(5000);
 	TD_LOG("td_timer_func called, %d; jiffies: %ld, tgt: %ld", td_counter++, jiffies, tgt);
 	if (td_counter < 10)
 	{
 		TD_LOG("td_timer_func, rescheduling");
-		mod_timer(&td_timer, tgt);
+		mod_timer(timer, tgt);
 	}
 	else
 	{
@@ -32,17 +41,24 @@ static void td_timer_func(struct timer_list* timer)
 	}
 }
 
+struct td_my_struct *ptr = NULL;
+
 static int __init td_module_init(void)
 {
 	TD_LOG("td_module_init: will create a timer");
 
 	unsigned long tgt = jiffies + msecs_to_jiffies(5000);
 
-	timer_setup(&td_timer, td_timer_func, 0);
+	ptr = kzalloc(sizeof(struct td_my_struct), GFP_KERNEL);
+	ptr->data1 = 667;
 
-	mod_timer(&td_timer, tgt);
+	//td_instance.data1 = 23;
 
-	TD_LOG("-%d-", &td_timer);
+	timer_setup(&ptr->tmr, td_timer_func, 0);
+	mod_timer(&ptr->tmr, tgt);
+	//timer_setup(&td_instance.tmr, td_timer_func, 0);
+	//mod_timer(&td_instance.tmr, tgt);
+
 	TD_LOG("jiffies: %ld, tgt: %ld", jiffies, tgt);
 
 	return 0;
@@ -50,7 +66,14 @@ static int __init td_module_init(void)
 
 static void __exit td_module_exit(void)
 {
-	del_timer(&td_timer);
+//	del_timer(&td_instance.tmr);
+
+	if(ptr)
+	{
+		del_timer(&ptr->tmr);
+		kfree(ptr);
+		ptr = NULL;
+	}
 	TD_LOG("td_module_exit");
 }
 
